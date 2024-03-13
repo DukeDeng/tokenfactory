15531b49b68a93f364d7c541369562f45677798d
samurai <samuraideng@pandaminer.com>
2024-03-13 17:21:39 +0800

    修改请求参数

--- x/tokenfactory/types/messages_denom.go
+++ x/tokenfactory/types/messages_denom.go
@@ -16,7 +16,6 @@ func NewMsgCreateDenom(
 	precision int32,
 	url string,
 	maxSupply int32,
-	supply int32,
 	canChangeMaxSupply bool,
 
 ) *MsgCreateDenom {
@@ -28,7 +27,6 @@ func NewMsgCreateDenom(
 		Precision:          precision,
 		Url:                url,
 		MaxSupply:          maxSupply,
-		Supply:             supply,
 		CanChangeMaxSupply: canChangeMaxSupply,
 	}
 }
@@ -37,6 +35,16 @@ func (msg *MsgCreateDenom) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Owner)
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid owner address (%s)", err)
+	}
+	tickerLength := len(msg.Ticker)
+	if tickerLength < 3 {
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidRequest, "Ticker length must be at least 3 chars long")
+	}
+	if tickerLength > 10 {
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidRequest, "Ticker length must be 10 chars long maximum")
+	}
+	if msg.MaxSupply == 0 {
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidRequest, "Max Supply must be greater than 0")
 	}
 	return nil
 }
@@ -47,11 +55,8 @@ func NewMsgUpdateDenom(
 	owner string,
 	denom string,
 	description string,
-	ticker string,
-	precision int32,
 	url string,
 	maxSupply int32,
-	supply int32,
 	canChangeMaxSupply bool,
 
 ) *MsgUpdateDenom {
@@ -59,11 +64,8 @@ func NewMsgUpdateDenom(
 		Owner:              owner,
 		Denom:              denom,
 		Description:        description,
-		Ticker:             ticker,
-		Precision:          precision,
 		Url:                url,
 		MaxSupply:          maxSupply,
-		Supply:             supply,
 		CanChangeMaxSupply: canChangeMaxSupply,
 	}
 }
@@ -72,6 +74,9 @@ func (msg *MsgUpdateDenom) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Owner)
 	if err != nil {
 		return errorsmod.Wrapf(sdkerrors.ErrInvalidAddress, "invalid owner address (%s)", err)
+	}
+	if msg.MaxSupply == 0 {
+		return errorsmod.Wrapf(sdkerrors.ErrInvalidRequest, "Max Supply must be greater than 0")
 	}
 	return nil
 }
